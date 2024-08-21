3173cb8fa449c60727a7a1857a32a67cb3d10125
Gonzalo Baez <gonzaasd7@gmail.com>
2024-08-21 11:06:11 -0300

    ajuste en el texto about

--- src/components/componentsAbout/about/AboutMe.jsx
+++ src/components/componentsAbout/about/AboutMe.jsx
@@ -8,7 +8,10 @@ export const AboutMe = () => {
                     <br />
                     🚀 Estoy familiarizado con los flujos de trabajo de Git, el uso de Trello para la gestión de proyectos y el marco de trabajo SCRUM, estructuras de datos: Listas enlazadas, pilas, colas, árboles binarios.
                     <br />
-                    🎯Soy autodidacta y busco ganar experiencia en el mundo Tech. Actualmente tomando cursos en Platzi</p>
+                    🎯Soy autodidacta y busco ganar experiencia en el mundo Tech. Actualmente tomando cursos en Platzi
+                    <br />
+                    Por mi horario de trabajo mañana y tarde/noche me es difícil ajustarme a una carrera informática, pero de entrar en la industria mi plan es hacer una tecnicatura en analisis de sistemas a la par del trabajo.
+                </p>
             </div>
         </section>
     )
