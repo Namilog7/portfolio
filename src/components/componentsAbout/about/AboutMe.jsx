0db2e1a753a9f1eda92a4277266ecec21fe46cce
Gonzalo Baez <gonzaasd7@gmail.com>
2025-04-05 10:53:03 -0300

    cambio en aboutMe

--- src/components/componentsAbout/about/AboutMe.jsx
+++ src/components/componentsAbout/about/AboutMe.jsx
@@ -4,11 +4,11 @@ export const AboutMe = () => {
     return (
         <section className={style.section} >
             <div className={style.content}>
-                <p>👨‍💻 Soy desarrollador Full Stack,he realizado Front-End con React, Redux Toolkit, Tailwind y Back-End con Node, Express, PostgreSQL, Sequelize.
+                <p>👨‍💻 Soy desarrollador Full Stack, y he realizado tanto Front-End con React, Redux Toolkit, Tailwind, como Back-End con Node, Express, PostgreSQL, Sequelize.
                     <br />
                     🚀 Estoy familiarizado con los flujos de trabajo de Git, el uso de Trello para la gestión de proyectos y el marco de trabajo SCRUM, estructuras de datos: Listas enlazadas, pilas, colas, árboles binarios.
                     <br />
-                    🎯Soy autodidacta y busco ganar experiencia en el mundo Tech. Actualmente trabajando de forma autónoma con colegas
+                    🎯Estudio Análisis de Sistemas, entusiasta de la tecnología, realizo trabajos de desarrollo web por mi cuenta, y me encanta aprender todo lo que sume a mi carrera profesional.
                 </p>
             </div>
         </section>
